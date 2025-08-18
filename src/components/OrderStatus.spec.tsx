5a40ef553e9d987f8295b8f5aa16f8466ab6d1ed
Kelvin Costa <okelvincosta@gmail.com>
2025-08-18 18:26:27 -0300

    Setup ambiente de testes

---
+++ src/components/OrderStatus.spec.tsx
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+
+// TODO: add modo de uso global sem import
+
+test("1 + 1 equqlas 2", () => {
+  expect(1 + 1).toBe(2);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
 });
+
+// Setup Test
+// install: vitest, testing-library for react, jest-dom, happy-dom
